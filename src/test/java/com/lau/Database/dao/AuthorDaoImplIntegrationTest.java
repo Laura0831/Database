bc472bc60c9f042a0fe70e34392ac9c968602c2a
Laura0831 <46638810+Laura0831@users.noreply.github.com>
2025-05-15 13:10:19 -0700

    added dirtiesContext to prevent multiple creation of same authors ID

--- src/test/java/com/lau/Database/dao/AuthorDaoImplIntegrationTest.java
+++ src/test/java/com/lau/Database/dao/AuthorDaoImplIntegrationTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
@@ -16,6 +17,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD) //this allows for spring boot to remove and
+// clean the database author creation after every test case is run. This prevents multiple creation of the same author
 public class AuthorDaoImplIntegrationTest {
 
 
